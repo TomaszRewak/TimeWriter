0c80d0902233b1da1185f45ff6f6309b585ecbde
Tomasz Rewak <tomaszrewak@gmail.com>
2018-12-08 19:19:57 +0100

    Moving from VS to VSC.

---
+++ time-writer-event-sourcing.Test/test.js
@@ -0,0 +1,5 @@
+﻿import Jasmine from 'jasmine';
+
+const jasmine = new Jasmine();
+jasmine.loadConfigFile('spec/support/jasmine.json');
+jasmine.execute();
---
+++ time-writer-event-sourcing.Test/tests/services/text-navigation-service-test.js
@@ -0,0 +1,27 @@
+﻿import TextNavigationService from './../../../TimeWriter.EventSourcing/services/text-navigation-service';
+
+describe('TextNavigationService test', function () {
+	it('countLines with empty string', function () {
+		const service = new TextNavigationService();
+
+		const lines = service.countLines('');
+		
+        expect(lines).toBe(1);
+    });
+
+	it('countLines with one line', function () {
+		const service = new TextNavigationService();
+
+		const lines = service.countLines('Some text');
+
+		expect(lines).toBe(1);
+	});
+
+	it('countLines with three line', function () {
+		const service = new TextNavigationService();
+
+		const lines = service.countLines('Some text \n More text \n And some more');
+
+		expect(lines).toBe(3);
+	});
+});
